57338fc757aa3ac5f434021cff58e9e4e13115c8
KrukUwr <43945852+KrukUwr@users.noreply.github.com>
2022-11-07 08:51:25 +0100

    Dodanie skryptu przyogtowującego dene do zad. lab3

---
+++ ListaZadan/lab3-data-preparation.R
@@ -0,0 +1,36 @@
+library(data.table)
+library(caret)
+
+load("../KrukUWr2022.RData")
+
+set.seed(123)
+
+samples <- caret::createFolds(
+  y = cases$CaseId, 
+  k=3)
+
+events[is.na(PaymentAmount), PaymentAmount:=0.0]
+
+payments_12m <- events[,.(
+    NumberOfPayments = sum(NumberOfPayment), 
+    SumOfPayments = sum(PaymentAmount)),
+  by = .(CaseId)]
+
+features_cols <- names(cases)
+targets_cols <- names(payments_12m)
+
+setkey(cases, CaseId)
+setkey(payments_12m, CaseId)
+
+cases_all <- cases[payments_12m]
+
+cases_train <- cases_all[samples[[1]], .SD, .SDcols = features_cols]
+cases_valid <- cases_all[samples[[2]], .SD, .SDcols = features_cols]
+cases_test <- cases_all[samples[[3]], .SD, .SDcols = features_cols]
+
+targets_train <- cases_all[samples[[1]], .SD, .SDcols = targets_cols]
+targets_valid <- cases_all[samples[[2]], .SD, .SDcols = targets_cols]
+targets_test <- cases_all[samples[[3]], .SD, .SDcols = targets_cols]
+
+rm(cases, events, payments_12m, features_cols, targets_cols, samples, cases_all)
+gc()
